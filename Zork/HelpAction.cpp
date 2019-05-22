e015853a69ed2b6f71684e27e82c8b058b143f17
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2019-05-22 04:13:31 +0200

    Improved HELP command

--- Zork/HelpAction.cpp
+++ Zork/HelpAction.cpp
@@ -6,15 +6,20 @@ HelpAction::HelpAction()
 {
 	generatedOutput = "Full list of commands:"
 		"\n"
-		"The 'INFO' command prints information which might give some idea of what the game is about.\n"
-		"The 'QUIT' command asks whether you wish to continue playing.\n"
-		"The 'INVENTORY' command lists the objects in your possession.\n"
+		"The 'HELP' displays the text that you are reading right now.\n"
+		"The 'SELF' command lists your stats, equipment and objects in your possession.\n"
 		"The 'LOOK' command prints a description of your surroundings.\n"
-		"The 'TIME' command tells you how long you have been playing.\n"
+		"The 'GO [NORTH|SOUTH|EAST|WEST]' command moves you to the chossen direction.\n"
+		"The '[TAKE|PICK] [ITEM]' command makes you pick an item from the floor.\n"
+		"The 'DROP [ITEM]' command makes you drop an item from the floor.\n"
+		"The 'PUT [ITEM] INTO [CONTAINER]' command makes you put an item into a container.\n"
+		"The 'CHECK [ITEM]' command shows you information of an item in your possesion.\n"
+		"The 'ATTACK [ENEMY]' command makes you attack an enemy with your equipped weapon.\n"
+		"The 'OPEN DOOR' command opens a door of the current room, if any. It consumes a key.\n"
 		"\n"
 		"Fighting:\n"
 		"\n"
-		"Zombies will, as a rule, try to eat you. In order to defend yourself you migh try the command 'ATTACK <zombie> WITH <weapon>', that may or may not be useful. Some weapons might require ammunation or fuel in order to work.\n";		
+		"The result of a fight is decided by the attack of the player and the defense of the enemy.\nIf attack is greater than the defense the enemy will be killed.\nOtherwise, player will lose a live.\nRunning out of lives means GAME OVER.\n";		
 }
 
 void HelpAction::execute()
